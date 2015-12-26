b387e7d8de1fac2ad3c6f6001e0a5c58c28e3c06
Wasif Chishti <wasifchishti@gmail.com>
2015-12-26 17:17:15 -0500

    Added ?completed filter

--- server.js
+++ server.js
@@ -15,9 +15,18 @@ app.get('/', function (req, res) {
 });
 
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.findWhere(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.findWhere(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 
