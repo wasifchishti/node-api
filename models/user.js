337404e1e6404bf652a92d1bfa479d27bb0b2596
Wasif Chishti <wasifchishti@gmail.com>
2015-12-27 15:18:14 -0500

    Add user model and POST /users request

---
+++ models/user.js
@@ -0,0 +1,20 @@
+module.exports = function(sequelize, DataTypes) {
+
+	return sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			}
+		}
+	});
+};
