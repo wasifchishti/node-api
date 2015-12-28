49f1c5c1aed0a45c31662a4751c3a1880d0906ce
Wasif Chishti <wasifchishti@gmail.com>
2015-12-27 23:02:30 -0500

    Adding DELETE /users/login

--- db.js
+++ db.js
@@ -20,6 +20,8 @@ var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
+db.token = sequelize.import(__dirname + '/models/token.js');
+
 db.sequelize = sequelize; // sequelize instance
 db.Sequelize = Sequelize; // Sequelize library
 
--- server.js
+++ server.js
@@ -154,18 +154,28 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if(token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-
-	}, function (e) {
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function (e) {
 		res.status(401).send();
+	});
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}, function (e) {
+		res.status(500).send();
 	});
 });
 
