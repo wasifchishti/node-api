91d468f03cbd8bc72e63428db4f765ee81bc8398
Wasif Chishti <wasifchishti@gmail.com>
2015-12-27 20:17:16 -0500

    Add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -5,95 +5,116 @@ var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 
-		var user = sequelize.define('user', {
-				email: {
-					type: DataTypes.STRING,
-					allowNull: false,
-					unique: true,
-					validate: {
-						isEmail: true
-					}
-				},
-				salt: {
-					type: DataTypes.STRING
-				},
-				password_hash: {
-					type: DataTypes.STRING
-				},
-				password: {
-					type: DataTypes.VIRTUAL,
-					allowNull: false,
-					validate: {
-						len: [7, 100]
-					},
-					set: function(value) {
-						var salt = bcrypt.genSaltSync(10);
-						var hashedPassword = bcrypt.hashSync(value, salt);
+	var user = sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
 
-						this.setDataValue('password', value);
-						this.setDataValue('salt', salt);
-						this.setDataValue('password_hash', hashedPassword);
-					}
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof(user.email) === 'string') {
+					user.email = user.email.toLowerCase();
 				}
-			}, {
-				hooks: {
-					beforeValidate: function(user, options) {
-						if (typeof(user.email) === 'string') {
-							user.email = user.email.toLowerCase();
-						}
+			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (!_.isString(body.email) || !_.isString(body.password)) {
+						return reject();
 					}
-				},
-				classMethods: {
-					authenticate: function(body) {
-						return new Promise(function(resolve, reject) {
-							if (!_.isString(body.email) || !_.isString(body.password)) {
-								return reject();
-							}
 
-							user.findOne({
-								where: {
-									email: body.email
-								}
-							}).then(function(user) {
-								if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-									return reject();
-								}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
 
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abd123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user){
+							if(user) {
 								resolve(user);
-							}, function(e) {
+							} else {
 								reject();
-							});
-						});
-					}
-				},
-				instanceMethods: {
-					toPublicJSON: function() {
-						var json = this.toJSON();
-						return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-					},
-					generateToken: function(type) {
-						if (!_.isString(type)) {
-							return undefined;
-						}
-
-						try {
-							var stringData = JSON.stringify({
-								id: this.get('id'),
-								type: type
-							});
-							var encryptedData = cryptojs.AES.encrypt(stringData, 'abd123!@#!').toString();
-							var token = jwt.sign({
-									token: encryptedData
-								}, 'qwerty098');
-
-								return token;
-							} catch (e) {
-								console.log(e);
-								return undefined;
 							}
-						}
+						}, function (e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
 					}
 				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
 
-			return user;
-		};
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abd123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					console.log(e);
+					return undefined;
+				}
+			}
+		}
+	});
+
+	return user;
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ app.get('/', function(req, res) {
 
 
 // GET /todos?completed=true&q=work
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -44,7 +45,7 @@ app.get('/todos', function(req, res) {
 
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoId).then(function(todo) {
@@ -61,7 +62,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 
 	var body = _.pick(req.body, 'description', 'completed');
 
@@ -73,7 +74,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -94,7 +95,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
